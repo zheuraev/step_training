ff35ceaf63e19226db40d7e4596d9e949dad510a
Zhenya Uraev <zheuraev@yandex.ru>
2021-03-23 21:49:36 +0500

    add sum func
    Added sum func with operator processing

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    pass
+def sum_func(a: int, b: int) -> int:
+    return a + b
 
+
+
+def main():
+
+    a = int(input('Введите первое число'))
+    operator = input('Введите оперпацию')
+    b = int(input('Введите второе число'))
+
+    if operator == '+':
+        print(sum_func(a, b))
 
 if __name__ == '__main__':
     main()
